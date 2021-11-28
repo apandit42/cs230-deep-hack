29e940db57aba7a6db4e1d0fc94f7636ffab905f
apandit42 <apandit@stanford.edu>
2021-11-28 04:19:49 -0800

    update tests

--- play.py
+++ play.py
@@ -2,23 +2,20 @@ from pathlib import Path
 import pickle
 import time
 import os
-import nle
-from nle.env.base import NLE
-import gym
-from simtest import TerminalStream
+from nle.nethack import tty_render
+from ansitoimg.render import ansiToSVG
 
-file = 'mon-hum-neu-mal 22:42:23 11-27-21.pickle'
+file = 'mon-hum-neu-mal 02:41:42 11-28-21.pickle'
 block = pickle.loads((Path('nh-runs') / Path(file)).read_bytes())
 
 frames = block['frame_counter']
 tty_chars_stack = block['chars_stack']
 tty_colors_stack = block['colors_stack']
 
-X = NLE()
-
 for chars, colors in zip(tty_chars_stack, tty_colors_stack):
-    ansi_text = X.get_tty_rendering(chars, colors)
-    
+    ansi_text = tty_render(chars, colors)
+    print(ansi_text)
+    ansiToSVG(ansi_text, 'test.svg')
     break
 
 # display.clear_output(wait=True)
